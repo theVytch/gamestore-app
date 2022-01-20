ed1fccb900d3ed6139f89f528283ef3de0398bc4
TheVytch <edu.vytch@gmail.com>
2022-01-20 20:53:15 -0300

    categoria-read foi implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { HomeComponent } from './components/views/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'categorias',
+    component: CategoriaReadComponent
   }
 ];
 
